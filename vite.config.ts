9a2983939d3ccc3ff9fc93b979a16c16a208b27e
Rajkumar Ganesan <raagaajava@yahoo.com>
2026-02-09 17:28:58 +0530

    Config Changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    lib: {
+      // Entry point must be your root index.ts
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "uicontrols",
+      fileName: (format) => `uicontrols.${format}.js`,
+      formats: ["es", "cjs"], // ensure both ES and CJS builds
+    },
+    rollupOptions: {
+      // Externalize peer dependencies so they aren’t bundled
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
   test: {
     globals: true,          // enables describe/it/expect without imports
     environment: 'jsdom',   // simulates browser DOM
